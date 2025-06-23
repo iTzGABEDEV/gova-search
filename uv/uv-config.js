a97ffc4c697f376af0ae8ad7ca4c0d5851b7163e
iTzGABEDEV <g337abriel@gmail.com>
2025-06-23 22:53:11 +0200

    Update uv-config.js

--- uv/uv-config.js
+++ uv/uv-config.js
@@ -1,6 +1,6 @@
-self.__uv$config = {
-  prefix: '/gova-search/uv/', // Replace with your real repo name
-  bare: 'https://itzgabedev.github.io/gova-search/', // Temporary public bare server
-  encodeUrl: Ultraviolet.codec.xor.encode,
-  decodeUrl: Ultraviolet.codec.xor.decode,
-};
+{
+  "prefix": "/uv/service/",
+  "bare": "https://bare.benrogo.net/",
+  "encodeUrl": "xor",
+  "decodeUrl": "xor"
+}
