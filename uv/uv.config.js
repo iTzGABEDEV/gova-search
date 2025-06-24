d6cb0f4cc560bea5873fe77f654e046b26b6214f
iTzGABEDEV <g337abriel@gmail.com>
2025-06-24 11:02:54 +0200

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-  prefix: '/gova-search/uv/service/',
-  bare: 'https://bare.benrogo.net/',
-  encodeUrl: Ultraviolet.codec.xor.encode,
-  decodeUrl: Ultraviolet.codec.xor.decode,
-  handler: '/gova-search/uv/uv.handler.js',
-  bundle: '/gova-search/uv/uv.bundle.js',
-  config: '/gova-search/uv/uv.config.js',
-  sw: '/gova-search/uv/uv.sw.js',
+    prefix: '/service/',
+    bare: '/bare/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
 };
